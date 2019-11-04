b63839416ba82dff3504b5ef620a7c7f493c6033
Rajarupan Sampanthan <rajarupan.sampanthan@mail.utoronto.ca>
2019-11-04 16:21:17 -0500

    Porting Lab 3 files

--- compiler467.c
+++ compiler467.c
@@ -1,9 +1,5 @@
 /***********************************************************************
- * Name: Yannan Lin
- *
- * Student Number: 1001257891
- *
- * Email: zzlyn.lin@mail.utoronto.ca
+ * **YOUR GROUP INFO SHOULD GO HERE**
  *
  * compiler467.c
  *
@@ -27,7 +23,7 @@
 #include "common.h"
 
 /* Phases 3,4: Uncomment following includes as needed */
-//#include "ast.h"
+#include "ast.h"
 //#include "codegen.h"
 
 /***********************************************************************
@@ -42,7 +38,7 @@
 #define DEFAULT_RUN_INPUT_FILE stdin
 
 void  getOpts   (int numargs, char **argstr);
-FILE *fileOpen  (char *fileName, const char *fileMode, FILE *defaultFile);
+FILE *fileOpen  (char *fileName, char *fileMode, FILE *defaultFile);
 void  sourceDump(void);
 
 /* Phase 1: Scanner Interface. For phase 2 and after these declarations
@@ -87,24 +83,26 @@ int main (int argc, char *argv[]) {
 
 /* Phase 2: Parser -- should allocate an AST, storing the reference in the
  * global variable "ast", and build the AST there. */
-  yyparse();
+  if(1 == yyparse()) {
+    return 0; // parse failed
+  }
 
 /* Phase 3: Call the AST dumping routine if requested */
- // if (dumpAST)
- //   printAST(ast);
+  if (dumpAST)
+    ast_print(ast);
 /* Phase 4: Add code to call the code generation routine */
 /* TODO: call your code generation routine here */
-//  if (errorOccurred)
-//    fprintf(outputFile,"Failed to compile\n");
-//  else 
-//    genCode(ast);
-
+  if (errorOccurred)
+    fprintf(outputFile,"Failed to compile\n");
+  else 
+   // genCode(ast);
+    ;
 /***********************************************************************
  * Post Compilation Cleanup
  **********************************************************************/
 
 /* Make calls to any cleanup or finalization routines here. */
-//	freeAST(ast);
+  ast_free(ast);
 
   /* Clean up files if necessary */
   if (inputFile != DEFAULT_INPUT_FILE)
@@ -188,11 +186,14 @@ void getOpts (int numargs, char **argstr) {
           }
           break;
         case 'O': /* Alternative output file */
+          printf("Blaaaaa\n");
           if (optarg[2] == 0) {
             i += 1;
             outputFile = fileOpen (argstr[i], "w", DEFAULT_OUTPUT_FILE);
+          printf("%s\n",argstr[i]);
           } else
             outputFile = fileOpen (&optarg[2], "w", DEFAULT_OUTPUT_FILE);
+          printf("%s\n",&optarg[2]);
           break;
         case 'E': /* Alternative error message file */
           if (optarg[2] == 0) {
@@ -237,7 +238,7 @@ void getOpts (int numargs, char **argstr) {
 /***********************************************************************
  * Utility for opening files 
  **********************************************************************/
-FILE *fileOpen (char *fileName, const char *fileMode, FILE *defaultFile) {
+FILE *fileOpen (char *fileName, char *fileMode, FILE *defaultFile) {
   FILE * fTemp;
 
   if ((fTemp = fopen (fileName, fileMode)) != NULL)
