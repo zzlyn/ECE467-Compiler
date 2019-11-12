d8fce200bc39a5abebda8cedcad14bdb80dd327b
Rajarupan Sampanthan <rajarupan.sampanthan@mail.utoronto.ca>
2019-11-12 16:38:54 -0500

    Adding chnages

--- symbol.cpp
+++ symbol.cpp
@@ -8,17 +8,35 @@
 
 using namespace std;
 
-vector<std::tr1::unordered_map<string,vector<int>>> symbolTable; 
+
+
+class varType {
+	public :  
+		int typeOfVariable;
+		bool isConst;
+        	varType(int given_type, bool given_const){typeOfVariable = given_type; isConst = given_const; }
+                varType(){typeOfVariable = -1; isConst = false;}
+
+};
+
+
+vector<std::tr1::unordered_map<string,varType>> symbolTable;
+extern "C" int getVarScope(char * varname);
 
 int my_scope_count = 0;
 
+
+
+// Call whenever you enter a new scope  
 extern "C" void addScope(){
 	my_scope_count += 1; 
-	std::tr1::unordered_map<string,vector<int>> scopeTable;
+	std::tr1::unordered_map<string,varType> scopeTable;
 	symbolTable.push_back(scopeTable);
         cout << endl << "Scope count is increased : " << my_scope_count << endl;  
 }
 
+
+// call whenever you exit a scope
 extern "C" void subtractScope(){
         my_scope_count -= 1;
 	symbolTable.pop_back();
@@ -26,35 +44,87 @@ extern "C" void subtractScope(){
 }
 
 
+// Finds out whether a varibale exists in the program
+extern "C" int doesVarExist(char * varname){
+ int currScope = my_scope_count - 1;
+        int varFound  = 0;
+        while(!varFound && currScope >= 0 ){
+                auto variableFound = symbolTable[currScope].find(varname);
+                if ( variableFound !=  symbolTable[currScope].end()){
+                	return 1;                        
+                }
+                else {
+                        currScope-=1;
+                }
+        }
+	return 0;
+}
+
+
+// Returns which scope the specific varibale name is declared in   
+extern "C" int getVarScope(char * varname){
+ int currScope = my_scope_count - 1;
+        int varFound  = 0; 
+        while(!varFound && currScope >= 0 ){
+                auto variableFound = symbolTable[currScope].find(varname);
+                if ( variableFound !=  symbolTable[currScope].end()){
+                        return currScope; 
+                }
+                else {
+                        currScope-=1;
+                }
+        }
+        return -1;
+}
+
+
+// Assumes that the varibale name exists somewhere
+extern "C" int  varTypeMatch(char * varname, int varType){
+
+	int varibaleScope = getVarScope(varname);
+        struct varType thisVarType = symbolTable[varibaleScope][varname];
+        int thisType = thisVarType.typeOfVariable;
+	if(thisType == varType){
+		return 1;
+	}
+	return 0; 
+}
+
+
+
+extern "C" int  getVarType(char * varname){
+        int varibaleScope = getVarScope(varname);
+        struct varType thisVarType = symbolTable[varibaleScope][varname];
+        int thisType = thisVarType.typeOfVariable;
+        return thisType;
+}
+
+
+extern "C" int  getConstType(char * varname){
+        int varibaleScope = getVarScope(varname);
+        struct varType thisVarType = symbolTable[varibaleScope][varname];
+        bool thisConst = thisVarType.isConst;
+        return thisConst;
+}
+
+
+extern "C" struct varType getVarStruct(char * varname){
+        int varibaleScope = getVarScope(varname);
+        struct varType thisVarType = symbolTable[varibaleScope][varname];
+        return thisVarType;
+}
+
+
+
 // Provide varibale name first and all the types after. Can take more than one. Fomrat for variable argument list. Number of arguments you want to look at
 // then the arguments. Make sure that the remaining number of arguments is atleast the number of variables you specified
 
-extern "C" void addToSymbolTable(char * var_name , ... ){
+extern "C" void addToSymbolTable(char * var_name , int given_varType , bool given_isConst ){
 	if (my_scope_count > 0){
 		auto variableFound = symbolTable[my_scope_count -1].find(var_name);
 		if (variableFound == symbolTable[my_scope_count - 1].end()){
-
-			char * varName = var_name;
-                        cout <<endl << "YOOOOOOOOOVar name is " << varName << endl;
-
-                        vector <int> emptyVector; 
-			symbolTable[my_scope_count - 1][varName] =  emptyVector;
-
-			va_list args;
-			va_start(args, var_name);
-			int numOfArgs = va_arg(args, int);
-
-			for(int i = 0; i < numOfArgs; i ++){
-                                int typeValue =  va_arg(args, int);
-                                symbolTable[my_scope_count - 1][var_name].push_back(typeValue);
-                                //cout << endl << "    The type is " << typeValue << endl;
-			}
-
-			//int numTypes = symbolTable[my_scope_count - 1][var_name].size();
-                        //cout << endl << "Num types is " << numTypes << endl;
-			
-
-			va_end(args);
+			varType thisVarType = varType(given_varType,given_isConst);
+                        symbolTable[my_scope_count - 1][var_name] = thisVarType; 
 
 		} 
 		else{
@@ -65,6 +135,7 @@ extern "C" void addToSymbolTable(char * var_name , ... ){
 
 
 // This will find the user symbol from the symbol table. Doesn't return anything for now. Not sure what ast implementation will look like. 
+/*
 extern "C" void findUsedSymbol(char * var_name){
 	int currScope = my_scope_count - 1;
 	int varFound  = 0;
@@ -88,7 +159,7 @@ extern "C" void findUsedSymbol(char * var_name){
 		cout << endl << "Error : Variable  " << var_name << " is not declared !" << endl;
 	}
 }
-
+*/
 
 
 
--- symbol.h
+++ symbol.h
@@ -9,15 +9,35 @@
 #ifndef SCOPE_CPP
 #define SCOPE_CPP
 
-
+// Call this when you enter a new scope
 extern "C"  void addScope();
+
+// Call this when you exit a scope
 extern "C"  void subtractScope();
 
+// Call this when you add a new symbol. Arguments are variable name and types. Types are integers
+extern "C" void addToSymbolTable(char * var_name , int given_varType , bool given_isConst );
+
+//  Call this to check if a var exists. 
+extern "C" int doesVarExist(char * varname);
 
 
-extern "C" void findUsedSymbol(char * var_name);
 
-extern "C" void addToSymbolTable(char * var_name , ... );
+
+// The following functions assume the variable name exists
+
+// Call this to check if variable type matches or not
+extern "C" int  varTypeMatch(char * varname, int varType);
+
+// Call this to get the varibale type of some var name
+extern "C" int  getVarType(char * varname);
+
+// Call this to get if a variable is constant or not
+extern "C" bool  getConstType(char * varname);
+
+// Call this function to get the struct vartype
+extern "C" struct varType getVarStruct(char * varname);
+
 
 #endif
 
