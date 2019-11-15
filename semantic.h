7b8b2bac3d39c245f9707fa283cf098aa0275221
zzlyn <zzlyn321@hotmail.com>
2019-11-15 13:59:16 -0500

    Created semantic check header file.

---
+++ semantic.h
@@ -0,0 +1,9 @@
+#include "ast.h"
+#include "symbol.h"
+
+// Check top-down the entire AST tree.
+bool semantic_check(AstNode* ast);
+
+// Check a specific AST node, also responsible for 
+// symbol creation & lookup.
+bool semantic_check_node(AstNode* node);
