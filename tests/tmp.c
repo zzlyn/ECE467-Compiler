044dbb79c64f5a5e3d76f17a57fd6ae034a5f9c5
zzlyn <zzlyn321@hotmail.com>
2019-11-19 19:52:12 -0500

    final version before submitting

--- tests/tmp.c
+++
@@ -1,5 +0,0 @@
-{   
-   const vec4 red = vec4(1.0,0.0,0.0,1.0);
-   const bool red_b = red[0];
-   const bool red_c = red;
-}
--- tests/unary_test_1.c
+++
@@ -1,8 +0,0 @@
-{
-    int a = 4;
-    bool b = false;
-    int aa = -a;
-    int bb = -b;
-    bool cc = !a;
-    bool dd = !b;
-}
